f6b8a372aa42527d40f3a0567a35be2bcaf11424
ariesdimas <ariesdimas@gmail.com>
2022-12-06 14:39:34 +0700

    chart calculator title

--- resources/views/userlog.blade.php
+++ resources/views/userlog.blade.php
@@ -4,8 +4,13 @@
    <title>User Log Chart</title>
 </head>
 <body>
+    <div class="row">
+        <div class="col">
+            <h3>Grafik Pemakaian Calculator</h3>
+            <canvas id="myChart" height="100px"></canvas>
+        </div>
+    </div>
 
-   <canvas id="myChart" height="100px"></canvas>
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" ></script>
 
